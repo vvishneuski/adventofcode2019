95bee1b416e893721f7ef9a0e37297df5e08d57b
vvishneuski <vishne.lospelicano@gmail.com>
2019-12-04 16:20:52 +0300

    Day4

---
+++ aoc2019/Day4.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2019
+{
+    public static class Day4
+    {
+        public static IEnumerable<int> GetDigits(this int number)
+        {
+            return GetDigitsInternal(number).Reverse();
+        }
+
+        private static IEnumerable<int> GetDigitsInternal(int number)
+        {
+            const int @base = 10;
+            while (number > 0)
+            {
+                yield return number % @base;
+                number /= @base;
+            }
+        }
+
+        public static IEnumerable<int> GetPossiblePasswords(string range)
+        {
+            return GetRange(range).Where(Applicable);
+        }
+
+        private static IEnumerable<int> GetRange(string range)
+        {
+            var limits = range.Split('-').Select(Int32.Parse).ToArray();
+            return Enumerable.Range(limits[0], limits[1] - limits[0] + 1);
+        }
+
+        private static bool Applicable(int number)
+        {
+            return number.DigitsAreNotDecrease() && number.HasTwoAdjacentDigitsTheSame();
+        }
+
+        public static bool DigitsAreNotDecrease(this int number)
+        {
+            return GetAdjacentDigitsPairs(number).All(pair => pair.i <= pair.j);
+        }
+
+        public static bool HasTwoAdjacentDigitsTheSame(this int number)
+        {
+            return GetAdjacentDigitsPairs(number).Any(pair => pair.i == pair.j);
+        }
+
+        private static IEnumerable<(int i, int j)> GetAdjacentDigitsPairs(int number)
+        {
+            var digits = number.GetDigits().ToList();
+
+            var firstDigits = digits.Take(digits.Count - 1);
+            var secondDigits = digits.Skip(1);
+
+            return firstDigits.Zip(secondDigits, (i, j) => (i, j));
+        }
+
+        public static IEnumerable<int> GetPossiblePasswordsAdvanced(string range)
+        {
+            return GetRange(range).Where(ApplicableAdvanced);
+        }
+
+        private static bool ApplicableAdvanced(int number)
+        {
+            return number.DigitsAreNotDecrease() && number.HasExactTwoDigitsTheSame();
+        }
+
+        public static bool HasExactTwoDigitsTheSame(this int number)
+        {
+            return number.GetDigits().GroupBy(digit => digit).Any(group => group.Count() == 2);
+        }
+    }
+}
---
+++ aoc2019/Day4TestSuite.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace aoc2019
+{
+    public class Day4TestSuite
+    {
+        public static TheoryData<int, IEnumerable<int>> GetDigitsData
+            => new TheoryData<int, IEnumerable<int>>
+            {
+                {111111, new[] {1, 1, 1, 1, 1, 1}},
+                {123456, new[] {1, 2, 3, 4, 5, 6}},
+                {567111, new[] {5, 6, 7, 1, 1, 1}},
+                {233456, new[] {2, 3, 3, 4, 5, 6}}
+            };
+
+        [Theory]
+        [MemberData(nameof(GetDigitsData))]
+        public void GetDigits(int number, IEnumerable<int> expectedDigits)
+        {
+            // Act
+            var actualDigits = number.GetDigits();
+            
+            // Assert
+            Assert.Equal(expectedDigits, actualDigits);
+        }
+
+        [Theory]
+        [InlineData(111111, true)]
+        [InlineData(123456, true)]
+        [InlineData(567111, false)]
+        [InlineData(233456, true)]
+        public void DigitsAreNotDecrease(int number, bool expected)
+        {
+            // Act
+            var actual = number.DigitsAreNotDecrease();
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(111111, true)]
+        [InlineData(123456, false)]
+        [InlineData(567111, true)]
+        [InlineData(233456, true)]
+        public void HasTwoAdjacentDigitsTheSame(int number, bool expected)
+        {
+            // Act
+            var actual = number.HasTwoAdjacentDigitsTheSame();
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPossiblePasswords()
+        {
+            // Arrange
+            var expected = 1650;
+            
+            // Act
+            var actual = Day4.GetPossiblePasswords("178416-676461").Count();
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(112233, true)]
+        [InlineData(123444, false)]
+        [InlineData(111122, true)]
+        public void HasExactTwoDigitsTheSame(int number, bool expected)
+        {
+            // Act
+            var actual = number.HasExactTwoDigitsTheSame();
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetPossiblePasswordsAdvanced()
+        {
+            // Arrange
+            var expected = 1129;
+            
+            // Act
+            var passwords = Day4.GetPossiblePasswordsAdvanced("178416-676461");
+            var actual = passwords.Count();
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
