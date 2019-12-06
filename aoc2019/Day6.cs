5d25443d3949c5126aa3a6f1ca7f2e1beea73640
vvishneuski <vishne.lospelicano@gmail.com>
2019-12-06 20:34:33 +0300

    Day6

---
+++ aoc2019/Day6.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2019
+{
+    public static class Day6
+    {
+        public class SpaceObject
+        {
+            public SpaceObject(string id, params SpaceObject[] satellites)
+            {
+                Id = id;
+                AddSatellites(satellites);
+            }
+
+            public string Id { get; }
+
+            public LinkedList<SpaceObject> Satellites { get; private set; }
+
+            public SpaceObject Orbits { get; private set; }
+
+            private void AddSatellites(IEnumerable<SpaceObject> satellites)
+            {
+                Satellites = new LinkedList<SpaceObject>(satellites);
+                foreach (var satellite in Satellites) satellite.Orbits = this;
+            }
+
+            private static int GetOrbitsCount(SpaceObject start, Predicate<SpaceObject> predicate)
+            {
+                var count = 0;
+                var orbits = start;
+                while (!predicate(orbits))
+                {
+                    count++;
+                    orbits = orbits.Orbits;
+                }
+
+                return count;
+            }
+
+            public int GetOrbitsCount()
+            {
+                return GetOrbitsCount(Orbits, orbits => orbits == null);
+            }
+
+            public int GetOrbitsCount(SpaceObject target)
+            {
+                return GetOrbitsCount(this, target.Equals);
+            }
+
+            public int GetOrbitsCount(string id)
+            {
+                return GetOrbitsCount(this, orbits => id.Equals(orbits.Id));
+            }
+
+            private static int GetSatellitesCount(SpaceObject start, int count, Predicate<SpaceObject> predicate)
+            {
+                if (predicate(start))
+                    return count;
+
+                if (!start.Satellites.Any())
+                    return -1;
+
+                count++;
+                return start.Satellites.Select(satellite => GetSatellitesCount(satellite, count, predicate)).Max();
+            }
+
+            public int GetSatellitesCount(SpaceObject target)
+            {
+                return GetSatellitesCount(this, 0, target.Equals);
+            }
+
+            public int GetSatellitesCount(string id)
+            {
+                return GetSatellitesCount(this, 0, orbits => id.Equals(orbits.Id));
+            }
+
+            private static int GetAllRoutesCount(SpaceObject start, int count)
+            {
+                count++;
+                return start.Satellites.Select(satellite => GetAllRoutesCount(satellite, count)).Sum() + count - 1;
+            }
+
+            public int GetAllRoutesCount()
+            {
+                return GetAllRoutesCount(this, 0);
+            }
+
+            private static IList<SpaceObject> GetSatellitesRoute(SpaceObject start, string destinationId,
+                IList<SpaceObject> route = null)
+            {
+                if (destinationId.Equals(start.Id))
+                    return route;
+
+                if (!start.Satellites.Any())
+                    return null;
+
+                route = new List<SpaceObject>(route ?? Enumerable.Empty<SpaceObject>())
+                {
+                    start
+                };
+                return start.Satellites.Select(satellite => GetSatellitesRoute(satellite, destinationId, route))
+                    .SingleOrDefault(r => r != null);
+            }
+
+            public int GetOrbitalTransfers(string startId, string finishId)
+            {
+                var startRoute = GetSatellitesRoute(this, startId);
+                var finishRoute = GetSatellitesRoute(this, finishId);
+
+                var commonRoute = startRoute.Intersect(finishRoute).ToList();
+
+                return startRoute.Count - commonRoute.Count
+                       + finishRoute.Count - commonRoute.Count;
+            }
+
+            public static SpaceObject ParseSpaceMap(string spaceMap)
+            {
+                var pairs = spaceMap.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(GetPair);
+                var objects = pairs.SelectMany(pair => new[] {pair.orbits, pair.satellite}).Distinct()
+                    .ToDictionary(id => id, id => new SpaceObject(id));
+                var orbits = pairs.GroupBy(pair => pair.orbits, pair => pair.satellite)
+                    .Select(group => GetOrbits(group, objects));
+                return orbits.ToList().Single(spaceObject => spaceObject.Orbits == null);
+            }
+
+            private static SpaceObject GetOrbits(IGrouping<string, string> group,
+                IReadOnlyDictionary<string, SpaceObject> objects)
+            {
+                var orbits = objects[group.Key];
+                orbits.AddSatellites(group.Select(id => objects[id]));
+                return orbits;
+            }
+
+            private static (string orbits, string satellite) GetPair(string pair)
+            {
+                var orbitsSatellite = pair.Trim().Split(')');
+                return (orbitsSatellite[0], orbitsSatellite[1]);
+            }
+        }
+    }
+}
---
+++ aoc2019/Day6TestSuite.cs
@@ -0,0 +1,125 @@
+﻿using aoc2019.Utils;
+using Xunit;
+
+namespace aoc2019
+{
+    public class Day6TestSuite
+    {
+        private static string LoadSpaceMap(string name)
+        {
+            using (var input = AssetManager.GetAsset(name))
+            {
+                return input.ReadToEnd();
+            }
+        }
+
+        [Fact]
+        public void GetOrbitalTransfers()
+        {
+            // Arrange
+            var spaceMap =
+                @"COM)B
+                    B)C
+                    C)D
+                    D)E
+                    E)F
+                    B)G
+                    G)H
+                    D)I
+                    E)J
+                    J)K
+                    K)L
+                    K)YOU
+                    I)SAN";
+
+            var expectedOrbitalTransfers = 4;
+
+            // Act
+            var COM = Day6.SpaceObject.ParseSpaceMap(spaceMap);
+            var actualOrbitalTransfers = COM.GetOrbitalTransfers("YOU", "SAN");
+
+            // Assert
+            Assert.Equal(expectedOrbitalTransfers, actualOrbitalTransfers);
+        }
+
+        [Fact]
+        public void GetOrbitsCount()
+        {
+            // Arrange
+            var E = new Day6.SpaceObject("E");
+            var D = new Day6.SpaceObject("D", E);
+            var C = new Day6.SpaceObject("C", D);
+            var A = new Day6.SpaceObject("A");
+            var B = new Day6.SpaceObject("B", A, C);
+            var COM = new Day6.SpaceObject("COM", B);
+
+            var expectedDBOrbits = 2;
+            var expectedAOrbits = 2;
+            var expectedCBOrbits = 1;
+            var expectedEOrbits = 4;
+
+            // Act
+            var actualDOrbits = D.GetOrbitsCount("B");
+            var actualAOrbits = A.GetOrbitsCount();
+            var actualCBOrbits = C.GetOrbitsCount(B);
+            var actualEOrbits = COM.GetSatellitesCount(E);
+
+            // Assert
+            Assert.Equal(expectedDBOrbits, actualDOrbits);
+            Assert.Equal(expectedAOrbits, actualAOrbits);
+            Assert.Equal(expectedCBOrbits, actualCBOrbits);
+            Assert.Equal(expectedEOrbits, actualEOrbits);
+        }
+
+        [Fact]
+        public void ParseSpaceMap()
+        {
+            // Arrange
+            var spaceMap =
+                @"COM)B
+                    B)C
+                    C)D
+                    D)E
+                    E)F
+                    B)G
+                    G)H
+                    D)I
+                    E)J
+                    J)K
+                    K)L";
+
+            var expectedId = "COM";
+            var expectedFOrbits = 5;
+            var expectedAllRoutes = 42;
+
+            // Act
+            var COM = Day6.SpaceObject.ParseSpaceMap(spaceMap);
+            var actualId = COM.Id;
+            var actualFOrbits = COM.GetSatellitesCount("F");
+            var actualAllRoutes = COM.GetAllRoutesCount();
+
+            // Assert
+            Assert.Equal(expectedId, actualId);
+            Assert.Equal(expectedFOrbits, actualFOrbits);
+            Assert.Equal(expectedAllRoutes, actualAllRoutes);
+        }
+
+        [Fact]
+        public void ParseSpaceMapFromInput()
+        {
+            // Arrange
+            var spaceMap = LoadSpaceMap("Day6_input");
+            var expectedAllRoutes = 223251;
+            var expectedOrbitalTransfers = 430;
+
+            // Act
+            var COM = Day6.SpaceObject.ParseSpaceMap(spaceMap);
+            var actualAllRoutes = COM.GetAllRoutesCount();
+            var actualOrbitalTransfers = COM.GetOrbitalTransfers("YOU", "SAN");
+
+            // Assert
+            Assert.Equal(expectedAllRoutes, actualAllRoutes);
+            Assert.Equal(expectedOrbitalTransfers, actualOrbitalTransfers);
+        }
+    }
+}
