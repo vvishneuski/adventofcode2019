6da296cba99b8f12edddb7dc7ed2b87aff1a4515
vvishneuski <vishne.lospelicano@gmail.com>
2019-12-10 20:29:49 +0300

    Day10

---
+++ aoc2019/Day10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2019
+{
+    public static class Day10
+    {
+        public class AsteroidField
+        {
+            public IDictionary<(int x, int y), bool> Map { get; }
+
+            private AsteroidField(IDictionary<(int x, int y), bool> map)
+            {
+                Map = new ConcurrentDictionary<(int x, int y), bool>(map);
+            }
+
+            public static AsteroidField ParseMap(string asteroidMap)
+            {
+                var rows = asteroidMap.Split(new []{Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToList();
+                var map = new Dictionary<(int x, int y), bool>();
+                for (var x = 0; x < rows.Count(); x++)
+                {
+                    for (var y = 0; y < rows[x].Length; y++)
+                    {
+                        map[(x, y)] = rows[x][y] == '#';
+                    }
+                }
+                return new AsteroidField(map);
+            }
+        }
+    }
+}
---
+++ aoc2019/Day10_TestSuite.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using Xunit;
+
+namespace aoc2019
+{
+    public class Day10_TestSuite
+    {
+        public static TheoryData<string, (int x, int y), int> GetBestLocationData
+            => new TheoryData<string, (int x, int y), int>
+            {
+                {
+                  @".#..#
+                    .....
+                    #####
+                    ....#
+                    ...##",
+                    (3, 4),
+                    8
+                },
+                {
+                  @"......#.#.
+                    #..#.#....
+                    ..#######.
+                    .#.#.###..
+                    .#..#.....
+                    ..#....#.#
+                    #..#....#.
+                    .##.#..###
+                    ##...#..#.
+                    .#....####",
+                    (5, 8),
+                    33
+                },
+                {
+                  @"#.#...#.#.
+                    .###....#.
+                    .#....#...
+                    ##.#.#.#.#
+                    ....#.#.#.
+                    .##..###.#
+                    ..#...##..
+                    ..##....##
+                    ......#...
+                    .####.###.",
+                    (1, 2),
+                    35
+                },
+                {
+                  @".#..#..###
+                    ####.###.#
+                    ....###.#.
+                    ..###.##.#
+                    ##.##.#.#.
+                    ....###..#
+                    ..#.#..#.#
+                    #..#.#.###
+                    .##...##.#
+                    .....#.#..",
+                    (6, 3),
+                    41
+                },
+                {
+                  @".#..##.###...#######
+                    ##.############..##.
+                    .#.######.########.#
+                    .###.#######.####.#.
+                    #####.##.#.##.###.##
+                    ..#####..#.#########
+                    ####################
+                    #.####....###.#.#.##
+                    ##.#################
+                    #####.##.###..####..
+                    ..######..##.#######
+                    ####.##.####...##..#
+                    .#####..#.######.###
+                    ##...#.##########...
+                    #.##########.#######
+                    .####.#.###.###.#.##
+                    ....##.##.###..#####
+                    .#.#.###########.###
+                    #.#.#.#####.####.###
+                    ###.##.####.##.#..##",
+                    (11, 13),
+                    210
+                },
+            };
+
+        [Theory]
+        [MemberData(nameof(GetBestLocationData))]
+        public void GetBestLocation(string asteroidMap, (int x, int y) expectedLocation, int expectedDetectedAsteroid)
+        {
+            // Arrange
+            
+            // Act
+            
+            // Assert
+        }
+
+        [Fact]
+        public void ParseAsteroidMap()
+        {
+            // Assert
+            var asteroidMap = @".#
+                                #.";
+
+            var expected10 = true;
+            var expected11 = false;
+            
+            // Act
+            var actualAsteroidField = Day10.AsteroidField.ParseMap(asteroidMap);
+
+            var actual10 = actualAsteroidField.Map[(1, 0)];
+            var actual11 = actualAsteroidField.Map[(1, 1)];
+
+            // Assert
+            Assert.Equal(expected10, actual10);
+            Assert.Equal(expected11, actual11);
+        }
+    }
+}
